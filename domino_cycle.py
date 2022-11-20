fc06de2936f31705d1361b5c0fb45de63b514219
granitelegend <daniel.alatalo.github@gmail.com>
2022-11-20 18:57:45 +0100

    Added algo-function and comments

--- domino_cycle.py
+++ domino_cycle.py
@@ -1,3 +1,22 @@
+"""
+Proposed solution for the domino_cycle problem from 109 Python Problems for CCPS
+"""
 
-test_domino = [(3, 5), (5, 2), (2, 3)]
 
+def domino_cycle(tiles):
+    """
+    Checks if a list of tuples statisfies a domino cycle.
+    :parameter tiles (list of 2-tuples)
+    :return boolean
+    """
+    if len(tiles) > 1:
+        for index, tile in enumerate(tiles):
+            if tile[1] != tiles[index + 1][0]:
+                return False
+            if index == len(tiles) - 2:
+                break
+    if not tiles:
+        return True
+    if tiles[-1][1] != tiles[0][0]:
+        return False
+    return True
