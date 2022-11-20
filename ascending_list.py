26b0ad8991a36aae5a0e6f01673192448c140796
granitelegend <daniel.alatalo.github@gmail.com>
2022-11-20 07:05:01 +0100

    initial commit

---
+++ ascending_list.py
@@ -0,0 +1,11 @@
+
+"""Proposed solution for the ascending_ problem from 109 Python Problems for CCPS"""
+
+def is_ascending(items):
+    """Checks if a list is strictly ascending"""
+    for counter, item in enumerate(items):
+        if counter == len(items) - 1:
+            break
+        if item >= items[counter + 1]:
+            return False
+    return True
---
+++ riffle_shuffle_kerfuffle.py
@@ -0,0 +1,25 @@
+"""soluiton"""
+
+
+def riffle(items, out=True):
+    """
+    Performs the Faro shuffle on a even list.
+    Unexecepted output if list is odd.
+    """
+
+    items_half_length = int(len(items) / 2)
+
+    items_first_half = items[:items_half_length]
+    items_second_half = items[items_half_length:]
+
+    if out:
+        for _ in range(items_half_length):
+            items.append(items_first_half.pop(0))
+            items.append(items_second_half.pop(0))
+    else:
+        for _ in range(items_half_length):
+            items.append(items_second_half.pop(0))
+            items.append(items_first_half.pop(0))
+
+    items = items[int(len(items) / 2):]
+    return items
