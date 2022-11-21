dd1578fb3ccc4ec0a8fabc8212e2b0bdccfe443a
granitelegend <daniel.alatalo.github@gmail.com>
2022-11-21 19:02:42 +0100

    Added another smaller solution, fixed comment for Solution 2

--- only_odd_digits.py
+++ only_odd_digits.py
@@ -3,9 +3,19 @@ Proposed solution for the only_odd_digits problem from 109 Python Problems for C
 """
 
 
+# Solution 1, attempt at single return statement solution
+def only_odd_digits(number):
+    """
+    Checks if number only contains odd digits
+    :param number
+    :return boolean
+    """
+    return not any(bool(int(digit) % 2 == 0) for digit in list(str(number)))
+
+# Solution 2, easy to read for-loop solution
 def only_odds_digits(number):
     """
-    Checks if number only contains even digits
+    Checks if number only contains odd digits
     :param number
     :return boolean
     """
