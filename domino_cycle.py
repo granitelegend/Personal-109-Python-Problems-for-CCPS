40d4c590d70231e0e9a10fe518a72f79a831c97c
granitelegend <daniel.alatalo.github@gmail.com>
2022-12-01 02:35:21 +0100

    New shorter solution, using try-except trick.

--- domino_cycle.py
+++ domino_cycle.py
@@ -4,19 +4,11 @@ Proposed solution for the domino_cycle problem from 109 Python Problems for CCPS
 
 
 def domino_cycle(tiles):
-    """
-    Checks if a list of tuples statisfies a domino cycle.
-    :parameter tiles (list of 2-tuples)
-    :return boolean
-    """
-    if len(tiles) > 1:
+    if tiles:
         for index, tile in enumerate(tiles):
-            if tile[1] != tiles[index + 1][0]:
-                return False
-            if index == len(tiles) - 2:
+            try:
+                if tiles[-1][1] != tiles[0][0] or tile[1] != tiles[index + 1][0]:
+                    return False
+            except IndexError:
                 break
-    if not tiles:
-        return True
-    if tiles[-1][1] != tiles[0][0]:
-        return False
     return True
