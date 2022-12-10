3b0a147f5c2386887a3932a25e56ed2b3b665b49
granitelegend <daniel.alatalo.github@gmail.com>
2022-12-10 18:48:05 +0100

    Added solution.

--- beat_the_previous.py
+++ beat_the_previous.py
@@ -1,2 +1,13 @@
-test_numbers = '045349'
+"""
+Proposed solution for the beat_the_previous problem from 109 Python Problems for CCPS
+"""
 
+
+def extract_increasing(digits):
+    results, previous_number, current_number = [], -1, 0
+    for d in digits:
+        current_number = 10 * current_number + int(d)
+        if previous_number < current_number:
+            results.append(current_number)
+            previous_number, current_number = current_number, 0
+    return results
